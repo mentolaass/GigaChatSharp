4953edf17f97516ef297953fc16b75a21f2a0d77
mentola <maximscherbakov006@gmail.com>
2023-10-30 00:17:22 +0300

    bugs fixed

--- GigaChatSharp/GigaChat.cs
+++ GigaChatSharp/GigaChat.cs
@@ -17,13 +17,16 @@ namespace GigaChatSharp
         private string GigaChatClientSecret { get; set; }
         private string GigaChatAuthData { get; set; }
         private Scope GigaChatScope { get; set; }
+        private bool IsAuthState { get; set; }
         private TokenAccessModel DataTokenAccess { get; set; }
         # endregion fields 
 
         #region events
         public delegate void AccessTokenExpired(AccessTokenArgs args);
+        public event AccessTokenExpired AccessTokenExpiredHandler;
 
-        public event AccessTokenExpired AccessTokenExpiredHandler;                      
+        private delegate void CallbackFunc();
+        private event CallbackFunc CallbackFuncHandler;
         #endregion events
 
         public GigaChat(string clientSecret, string authData, Scope scope)
@@ -33,11 +36,24 @@ namespace GigaChatSharp
             GigaChatClientSecret = clientSecret;
             GigaChatAuthData = authData;
             GigaChatScope = scope;
+
+            CallbackFuncHandler += GigaChat_CallbackFuncHandler;
         }
 
-        private bool IsTokenActive()
+        private void GigaChat_CallbackFuncHandler()
         {
-            return DateTime.Now > new DateTime(DataTokenAccess.ExpiresDate);
+            if (DataTokenAccess is null)
+            {
+                throw new Exception("Please auth to use this api.");
+            }
+        }
+
+        private readonly DateTime UnixEpoch =
+            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local);
+
+        public DateTime GetAccessTokenExpDate()
+        {
+            return UnixEpoch.AddMilliseconds(DataTokenAccess.ExpiresDate);
         }
 
         public async Task Authorize()
@@ -48,33 +64,38 @@ namespace GigaChatSharp
                 {
                     message.Headers.Add("Authorization", $"Bearer {GigaChatAuthData}");
                     message.Headers.Add("RqUID", GigaChatClientSecret);
-
-                    message.Content = new StringContent($"scope={GigaChatScope.ToString()}");
+                    message.Content = new StringContent($"scope={GigaChatScope}");
                     message.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-
                     var response = await client.SendAsync(message);
                     DataTokenAccess = JsonConvert.DeserializeObject<TokenAccessModel>(await response.Content.ReadAsStringAsync());
+                    IsAuthState = false;
                 }
+            }
+        }
+
+        private void HandleAccessTokenExpire()
+        {
+            if ((long)DateTime.UtcNow.Subtract(DateTime.Now).TotalMilliseconds >= DataTokenAccess.ExpiresDate)
+            {
+                IsAuthState = true;
+                AccessTokenExpiredHandler.Invoke(new AccessTokenArgs() { GigaChatClient = this });
             }
         }
 
         public async Task<ListModel> GetModels()
         {
-            if (!IsTokenActive())
-            {
-                AccessTokenExpiredHandler.Invoke(new AccessTokenArgs() { GigaChatClient = this });
+            CallbackFuncHandler.Invoke();
+            HandleAccessTokenExpire();
+            if (IsAuthState)
                 return null;
-            }
 
             using (HttpClient client = new HttpClient())
             {
                 using (HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "https://gigachat.devices.sberbank.ru/api/v1/models"))
                 {
                     message.Headers.Add("Authorization", $"Bearer {DataTokenAccess.AccessToken}");
-
                     var response = await client.SendAsync(message);
                     var data = JsonConvert.DeserializeObject<ListModel>(await response.Content.ReadAsStringAsync());
-
                     return data;
                 }
             }
@@ -82,24 +103,20 @@ namespace GigaChatSharp
 
         public async Task<ResponseQueryModel> SendMessage(ParametersModel dataParams, string answer)
         {
-            if (!IsTokenActive())
-            {
-                AccessTokenExpiredHandler.Invoke(new AccessTokenArgs() { GigaChatClient = this });
+            CallbackFuncHandler.Invoke();
+            HandleAccessTokenExpire();
+            if (IsAuthState)
                 return null;
-            }
 
             dataParams.Messages = dataParams.Messages.Append(new MessageQueryModel() { Role="user", Content=answer }).ToArray();
-
             using (HttpClient client = new HttpClient())
             {
                 using (HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"))
                 {
                     message.Headers.Add("Authorization", $"Bearer {DataTokenAccess.AccessToken}");
                     message.Content = new StringContent(JsonConvert.SerializeObject(dataParams), Encoding.UTF8);
-
                     var response = await client.SendAsync(message);
                     var data = JsonConvert.DeserializeObject<ResponseQueryModel>(await response.Content.ReadAsStringAsync());
-
                     return data;
                 }
             }
